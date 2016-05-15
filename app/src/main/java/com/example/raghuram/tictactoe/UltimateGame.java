e168be79257b49dbce57bc5c6ebb16cca999cf4d
Achilles-96 <raghuram.vadapalli@research.iiit.ac.in>
2016-05-15 16:35:40 +0530

    Added moves

--- app/src/main/java/com/example/raghuram/tictactoe/UltimateGame.java
+++ app/src/main/java/com/example/raghuram/tictactoe/UltimateGame.java
@@ -2,12 +2,135 @@ package com.example.raghuram.tictactoe;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class UltimateGame extends ActionBarActivity {
+public class UltimateGame extends ActionBarActivity implements View.OnClickListener {
+
+    private int[] map_of_open_moves = new int[82];
+    private int turn = 1;
+    private static final int BLOCKED = 0;
+    private static final int OPEN = 1;
+    private static final int PLAYER1 = 2;
+    private static final int PLAYER2 = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultimate_game);
+        for (int i = 1; i <= 81; i++) {
+            setOpen(i);
+        }
+        for (int i = 1; i <= 81; i++) {
+            int id = getResources().getIdentifier("button" + i, "id", getPackageName());
+            View view = findViewById(id);
+            view.setAlpha(1);
+            view.setOnClickListener(this);
+        }
+    }
+
+    public void onClick(View view) {
+        String IdAsString = view.getResources().getResourceName(view.getId());
+        int id = Integer.parseInt(IdAsString.substring(40));
+        if (map_of_open_moves[id] != OPEN) {
+            return;
+        }
+        if (turn == 1) {
+            ((Button) view).setText("X");
+            turn = 2;
+            map_of_open_moves[id] = PLAYER1;
+        } else {
+            ((Button) view).setText("0");
+            map_of_open_moves[id] = PLAYER2;
+            turn = 1;
+        }
+        updateMap(id);
+        updateAlphas();
+    }
+
+    private void setOpen(int id) {
+        if (map_of_open_moves[id] != PLAYER1 && map_of_open_moves[id] != PLAYER2)
+            map_of_open_moves[id] = OPEN;
+    }
+
+    private void updateMap(int id) {
+        for (int i = 1; i <= 81; i++) {
+            if (map_of_open_moves[i] == OPEN)
+                map_of_open_moves[i] = BLOCKED;
+        }
+        switch (id % 9) {
+            case 1:
+                for (int i = 10, j = 28; i <= 18; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 2:
+                for (int i = 1, j = 19; i <= 9; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 3:
+                for (int i = 10, j = 46; i <= 18; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 4:
+                for (int i = 1, j = 55; i <= 9; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 5:
+                for (int i = 37; i <= 45; i++) {
+                    setOpen(i);
+                }
+                break;
+            case 6:
+                for (int i = 19, j = 73; i <= 27; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 7:
+                for (int i = 28, j = 64; i <= 36; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 8:
+                for (int i = 55, j = 73; i <= 63; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+            case 0:
+                for (int i = 46, j = 64; i <= 54; i++, j++) {
+                    setOpen(i);
+                    setOpen(j);
+                }
+                break;
+        }
+    }
+
+    private void updateAlphas() {
+        for (int i = 1; i <= 81; i++) {
+            int id = getResources().getIdentifier("button" + i, "id", getPackageName());
+            View view = findViewById(id);
+            switch (map_of_open_moves[i]) {
+                case BLOCKED:
+                    view.setAlpha((float) 0.7);
+                    break;
+                case OPEN:
+                    view.setAlpha((float) 1);
+                    break;
+                case PLAYER1:
+                case PLAYER2:
+                    view.setAlpha((float) 0.4);
+                    break;
+            }
+        }
     }
 }

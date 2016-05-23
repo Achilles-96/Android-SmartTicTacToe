1efb5fcf7a95a2469c1a1d4b277add94441f53ba
Achilles-96 <raghuram.vadapalli@research.iiit.ac.in>
2016-05-23 23:52:19 +0530

    Added some rule

--- app/src/main/java/com/example/raghuram/tictactoe/UltimateGame.java
+++ app/src/main/java/com/example/raghuram/tictactoe/UltimateGame.java
@@ -2,6 +2,7 @@ package com.example.raghuram.tictactoe;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,8 @@ public class UltimateGame extends ActionBarActivity implements View.OnClickListe
     private static final int OPEN = 1;
     private static final int PLAYER1 = 2;
     private static final int PLAYER2 = 3;
+    private int game_state = 1;
+    private int markers[] = new int[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +30,14 @@ public class UltimateGame extends ActionBarActivity implements View.OnClickListe
             view.setAlpha(1);
             view.setOnClickListener(this);
         }
+        for(int i = 1;i<=9;i++)
+            markers[i] = -1;
     }
 
     public void onClick(View view) {
         String IdAsString = view.getResources().getResourceName(view.getId());
         int id = Integer.parseInt(IdAsString.substring(40));
-        if (map_of_open_moves[id] != OPEN) {
+        if (map_of_open_moves[id] != OPEN || game_state == 0 || markers[((id-1)/9)+1] != -1) {
             return;
         }
         if (turn == 1) {
@@ -44,8 +49,108 @@ public class UltimateGame extends ActionBarActivity implements View.OnClickListe
             map_of_open_moves[id] = PLAYER2;
             turn = 1;
         }
+        int boardStatus = checkWin();
+        Log.d("board status",boardStatus+"");
+        if(boardStatus != -1){
+            game_state = 0;
+            return;
+        }
         updateMap(id);
         updateAlphas();
+    }
+
+    private int checkWin() {
+
+        int start, end;
+        for(int i = 1;i<=9;i++){
+            start = i*9 - 8;
+            end = start + 9 - 1;
+            markers[i] = checkBlock(start, end);
+            Log.d("Marker" + i, markers[i]+"");
+        }
+        start = 1;
+        end = 9;
+        for(int i=start;i<end;i+=3)
+            if(markers[i] == PLAYER1 &&
+                    markers[i + 1] == PLAYER1 &&
+                    markers[i + 2] == PLAYER1
+                    )
+                return PLAYER1;
+        for(int i=start;i<end;i+=3)
+            if(markers[i] == PLAYER2 &&
+                    markers[i + 1] == PLAYER2 &&
+                    markers[i + 2] == PLAYER2
+                    )
+                return PLAYER2;
+        for (int i = start; i <= start + 2; i++)
+            if (markers[i] == PLAYER1 &&
+                    markers[i+3] == PLAYER1 &&
+                    markers[i+6] == PLAYER1)
+                return PLAYER1;
+        for (int i = start; i <= start + 2; i++)
+            if (markers[i] == PLAYER2 &&
+                    markers[i+3] == PLAYER2 &&
+                    markers[i+6] == PLAYER2)
+                return PLAYER2;
+        if(markers[start] == PLAYER1 &&
+                markers[start + 4] == PLAYER1 &&
+                markers[start + 8] == PLAYER1)
+            return PLAYER1;
+        if(markers[start] == PLAYER2 &&
+                markers[start + 4] == PLAYER2 &&
+                markers[start + 8] == PLAYER2)
+            return PLAYER2;
+        if(markers[start + 2] == PLAYER1 &&
+                markers[start + 4] == PLAYER1 &&
+                markers[start + 6] == PLAYER1)
+            return PLAYER1;
+        if(markers[start + 2] == PLAYER2 &&
+                markers[start + 4] == PLAYER2 &&
+                markers[start + 6] == PLAYER2)
+            return PLAYER2;
+        return -1;
+    }
+
+    private int checkBlock(int start, int end) {
+        for(int i=start;i<end;i+=3)
+            if(map_of_open_moves[i] == PLAYER1 &&
+                    map_of_open_moves[i + 1] == PLAYER1 &&
+                    map_of_open_moves[i + 2] == PLAYER1
+                    )
+                return PLAYER1;
+        for(int i=start;i<end;i+=3)
+            if(map_of_open_moves[i] == PLAYER2 &&
+                    map_of_open_moves[i + 1] == PLAYER2 &&
+                    map_of_open_moves[i + 2] == PLAYER2
+                    )
+                return PLAYER2;
+        for (int i = start; i <= start + 2; i++)
+            if (map_of_open_moves[i] == PLAYER1 &&
+                    map_of_open_moves[i+3] == PLAYER1 &&
+                    map_of_open_moves[i+6] == PLAYER1)
+                return PLAYER1;
+        for (int i = start; i <= start + 2; i++)
+            if (map_of_open_moves[i] == PLAYER2 &&
+                    map_of_open_moves[i+3] == PLAYER2 &&
+                    map_of_open_moves[i+6] == PLAYER2)
+                return PLAYER2;
+        if(map_of_open_moves[start] == PLAYER1 &&
+                map_of_open_moves[start + 4] == PLAYER1 &&
+                map_of_open_moves[start + 8] == PLAYER1)
+            return PLAYER1;
+        if(map_of_open_moves[start] == PLAYER2 &&
+                map_of_open_moves[start + 4] == PLAYER2 && 
+                map_of_open_moves[start + 8] == PLAYER2)
+            return PLAYER2;
+        if(map_of_open_moves[start + 2] == PLAYER1 &&
+                map_of_open_moves[start + 4] == PLAYER1 &&
+                map_of_open_moves[start + 6] == PLAYER1)
+            return PLAYER1;
+        if(map_of_open_moves[start + 2] == PLAYER2 &&
+                map_of_open_moves[start + 4] == PLAYER2 &&
+                map_of_open_moves[start + 6] == PLAYER2)
+            return PLAYER2;
+        return -1;
     }
 
     private void setOpen(int id) {
